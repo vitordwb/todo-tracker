9a1cf6126b44449c785b2948517896a7c1489a29
Vitor <vitordwb@gmail.com>
2023-07-06 20:40:05 -0300

    adding method to disable buttons

--- src/components/StartStopTimer.vue
+++ src/components/StartStopTimer.vue
@@ -3,13 +3,13 @@
     class="is-flex is-align-items-center is-justify-content-space-between"
   >
     <StopwatchTimer :timeInSeconds="timeInSeconds"/>
-    <button class="button" @click="start">
+    <button class="button" @click="start" :disabled="stopwatchRunning">
       <span class="icon">
         <i class="fas fa-play"></i>
       </span>
       <span>play</span>
     </button>
-    <button class="button" @click="stop">
+    <button class="button" @click="stop" :disabled="!stopwatchRunning">
       <span class="icon">
         <i class="fas fa-stop"></i>
       </span>
@@ -24,23 +24,29 @@ import StopwatchTimer from '@/components/StopwatchTimer.vue'
 
 export default defineComponent({
   name: 'StartStopTimer',
+  emits: ['toTimerFinish'],
   components: {
     StopwatchTimer
   },
   data() {
     return {
       timeInSeconds: 0,
-      stopwatch: 0
+      stopwatch: 0,
+      stopwatchRunning: false,
     }
   },
   methods: {
     start() {
+      this.stopwatchRunning = true;
       this.stopwatch = setInterval(()=> {
         this.timeInSeconds++
-      }, 1000)
+      }, 1000);
     },
     stop() {
+      this.stopwatchRunning = false;
       clearInterval(this.stopwatch);
+      this.$emit('toTimerFinish', this.timeInSeconds);
+      this.timeInSeconds = 0;
     },
   }
 })
