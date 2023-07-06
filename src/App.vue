b8cdb0c80f6f8b3fe5d70118ed07801806148181
Vitor <vitordwb@gmail.com>
2023-07-06 16:17:31 -0300

    creating InputForm component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
   <main class="columns is-gapless is-multiline">
     <div class="colum is-one-quarter">
-    <BarraLateral />
+      <BarraLateral />
     </div>
     <div class="colum is-three-quarter">
-
+      <InputForm />
     </div>
   </main>
 </template>
@@ -12,10 +12,12 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import BarraLateral from '@/components/BarraLateral.vue'
+import InputForm from '@/components/InputForm.vue'
 
 export default defineComponent({
   name: 'App',
   components: {
+    InputForm,
     BarraLateral
   },
 });
---
+++ src/components/InputForm.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="columns is-8" role="form" aria-label="Formulário para criaçao de uma nova tarefa">
+        <input type="text" class="input" placeholder="Qual tarefa deseja iniciar?">
+      </div>
+      <div class="column">
+        <section>
+          <strong>00:00:00</strong>
+        </section>
+        <button class="button">
+          <span class="icon">
+            <i class="fa fas-play"></i>
+          </span>
+          <span>play</span>
+        </button>
+        <button class="button">
+          <span class="icon">
+            <i class="fa fas-stop"></i>
+          </span>
+          <span>stop</span>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'InputForm'
+})
+</script>
