0f7d7f4cb45f27d8bf155ad4d77697464bfc2087
Vitor <vitordwb@gmail.com>
2023-07-06 17:23:41 -0300

    adding functions

--- src/components/InputForm.vue
+++ src/components/InputForm.vue
@@ -1,31 +1,35 @@
 <template>
   <div class="box">
     <div class="columns">
-      <div class="columns is-8"
+      <div class="column is-8"
            role="form"
-           aria-label="Formulário para criaçao de uma nova tarefa"
+           aria-label="Form to create a new task"
       >
         <input type="text"
                class="input"
-               placeholder="Qual tarefa deseja iniciar?"
+               placeholder="Which task you want to star?"
         />
       </div>
       <div class="column">
-        <section>
-          <strong>00:00:00</strong>
-        </section>
-        <button class="button">
+        <div
+            class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>{{ timeSpent }}</strong>
+          </section>
+          <button class="button" @click="start">
           <span class="icon">
-            <i class="fa fas-play"></i>
+            <i class="fas fa-play"></i>
           </span>
-          <span>play</span>
-        </button>
-        <button class="button">
+            <span>play</span>
+          </button>
+          <button class="button" @click="stop">
           <span class="icon">
-            <i class="fa fas-stop"></i>
+            <i class="fas fa-stop"></i>
           </span>
-          <span>stop</span>
-        </button>
+            <span>stop</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -35,6 +39,27 @@
 import { defineComponent } from 'vue'
 
 export default defineComponent({
-  name: 'InputForm'
+  name: 'InputForm',
+  data() {
+    return {
+      timeInSeconds: 0,
+      stopwatch: 0
+    }
+  },
+  computed: {
+    timeSpent(): string {
+      return new Date(this.timeInSeconds * 1000).toISOString().substr(11, 8);
+    }
+  },
+  methods: {
+    start() {
+      this.stopwatch = setInterval(()=> {
+        this.timeInSeconds++
+      }, 1000)
+    },
+    stop() {
+      clearInterval(this.stopwatch);
+    },
+  }
 })
 </script>
